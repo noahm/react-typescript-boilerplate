88e436e656caa75c0cbcd74d8273997d3c5fb3e5
Noah Manneschmidt <noah@manneschmidt.net>
2017-05-07 22:34:38 -0700

    Fix build script

--- scripts/build.js
+++ scripts/build.js
@@ -4,7 +4,7 @@ const child_process = require('child_process');
 const process = require('process');
 const path = require('path');
 
-const cmd = path.join("node_modules",".bin","webpack") + " --progress --profile --colors --optimize-minimize";
+const cmd = path.join("node_modules",".bin","webpack") + " --config webpack-prod.config.js --progress --profile --colors --optimize-minimize";
 
 process.cwd('..');
 child_process.execSync(cmd, {
